0c1d62924ba5fc3771328a9959a6fc5226b6e144
ll3006 <doublel3006@gmail.com>
2025-04-08 04:12:57 +0200

    refactor!: move config util functions to ProviderConfig
    BREAKING CHANGE: removed self.utils from ProviderConfig. Directly
    use self.fetch and self.proxy instead.

--- src/jidouteki/config/__init__.py
+++ src/jidouteki/config/__init__.py
@@ -11,39 +11,6 @@ import typing
 if typing.TYPE_CHECKING:
     from .. import Jidouteki
     
-class ProviderConfigUtils():
-    def __init__(self, config: "ProviderConfig") -> None:
-        self.config = config
-    
-    @property
-    def provider(self): 
-        from ..provider.provider import Provider
-        return Provider(self.config)
-    
-    def fetch(self, *args, method="GET", **kwargs) -> FetchedData:
-        if self.provider.meta.base: args = (self.provider.meta.base, *args) 
-        args = [arg if isinstance(arg, (list,tuple)) else [arg] for arg in args]
-        urls = [reduce(lambda a, b: urljoin(a, b), p) for p in itertools.product(*args)]
-        
-        contents = []
-        for url in urls:
-            resp = self.config.session.request(method, url, **kwargs)
-            if not resp.ok: continue
-            contents.append(resp.content)
-        return FetchedData(contents)
-
-
-    def proxy(self, url, headers: Dict[str, str]={}):
-        params = []
-        for key,val in headers.items():
-            header = ''.join([w.capitalize() for w in key.split('-')])
-            params.append(('header', f"{header}: {val}"))
-    
-        params.append(("url", url))
-        query_string = urllib.parse.urlencode(params)
-            
-        return f"{self.config.context.proxy}?{query_string}"
-
 class ProviderConfig(ABC):
     """
     The configuration for a single scraper. Each website should extend 
@@ -60,7 +27,6 @@ class ProviderConfig(ABC):
     def __init__(self, context: "Jidouteki") -> None:
         self.context = context
         self.session = CachedSession(expire_after=self.context.cache_ttl, backend="memory")
-        self.utils = ProviderConfigUtils(self)
         
     def __init_subclass__(cls, **kwargs):
         cls.__MAPPINGS = {}
@@ -72,3 +38,31 @@ class ProviderConfig(ABC):
     def _get(self, key):
         return self.__MAPPINGS.get(key)
     
+    @property
+    def provider(self): 
+        from ..provider.provider import Provider
+        return Provider(self)
+    
+    def fetch(self, *args, method="GET", **kwargs) -> FetchedData:
+        if self.meta.base: args = (self.meta.base, *args) 
+        args = [arg if isinstance(arg, (list,tuple)) else [arg] for arg in args]
+        urls = [reduce(lambda a, b: urljoin(a, b), p) for p in itertools.product(*args)]
+        
+        contents = []
+        for url in urls:
+            resp = self.session.request(method, url, **kwargs)
+            if not resp.ok: continue
+            contents.append(resp.content)
+        return FetchedData(contents)
+
+
+    def proxy(self, url, headers: Dict[str, str]={}):
+        params = []
+        for key,val in headers.items():
+            header = ''.join([w.capitalize() for w in key.split('-')])
+            params.append(('header', f"{header}: {val}"))
+    
+        params.append(("url", url))
+        query_string = urllib.parse.urlencode(params)
+            
+        return f"{self.context.proxy}?{query_string}"
